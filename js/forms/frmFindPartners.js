cc5fb866147307ef7c8125ebdca3aadb8ce98eda
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-02-18 07:36:05 +0100

    new javascript function ComboboxInitValues

--- js/forms/frmFindPartners.js
+++ js/forms/frmFindPartners.js
@@ -85,48 +85,14 @@ function Init() {
         }
     });
 
-    $.ajax({
-      type: "POST",
-      url: "serverMCommon.asmx/TCommonCacheableWebConnector_GetCacheableTable",
-      data: JSON.stringify({
-            'ACacheableTable': 'CountryList', 
-            'AHashCode': '',
-            }),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data, status, response) {
-        result = JSON.parse(response.responseText);
-        //console.debug(JSON.stringify(response.responseText));
-        //console.debug(JSON.stringify(result));
-        if (result['d'] != 0)
-        {
-            //console.debug(JSON.stringify(result['d']));
-            //console.debug(JSON.stringify(JSON.parse(result['d'])));
-            
-            SearchResult = JSON.parse(result['d'])[1];
-            
-            ParsedSearchResult = new Array();
-            
-            options = "<option></option>";
-            for (index = 0; index < SearchResult.length; ++index) {
-                options += "<option value='" + SearchResult[index]["p_country_code_c"] + "'>" + 
-                    SearchResult[index]["p_country_name_c"] + "</option>";
-            }
-            $("#country").html(options);
-        }
-        else
-        {
-            alert("something went wrong");
-        }
-      },
-      error: function(response, status, error) {
-        console.debug(error);
-        console.debug(JSON.stringify(response.responseJSON));
-        alert("Server error, please try again later");
-      },
-      fail: function(msg) {
-        console.debug(msg);
-        alert("Server failure, please try again later");
-      }
-    });      
+    ComboboxInitValues(
+                    "serverMCommon.asmx/TCommonCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'CountryList', 
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_country_code_c",
+                    "p_country_name_c",
+                    "#country");
 }
