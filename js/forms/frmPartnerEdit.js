12521818978a57740eaa9e687cb9c18118978eac
Haeder <haeder@solidcharity.com>
2014-02-21 14:11:34 +0100

    add combobox values in Partner Edit

--- js/forms/frmPartnerEdit.js
+++ js/forms/frmPartnerEdit.js
@@ -1,140 +1,75 @@
 function Init() {
 
 // Partner Status
-    $.ajax({
-      type: "POST",
-      url: "serverMPartner.asmx/TPartnerCacheableWebConnector_GetCacheableTable",
-      data: JSON.stringify({
-            'ACacheableTable': 'PartnerStatusList',
-            'AHashCode': '',
-            }),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data, status, response) {
-        result = JSON.parse(response.responseText);
-        //console.debug(JSON.stringify(response.responseText));
-        //console.debug(JSON.stringify(result));
-        if (result['d'] != 0)
-        {
-            //console.debug(JSON.stringify(result['d']));
-            //console.debug(JSON.stringify(JSON.parse(result['d'])));
-
-            SearchResult = JSON.parse(result['d'])[1];
-
-            ParsedSearchResult = new Array();
-
-            options = "";
-            for (index = 0; index < SearchResult.length; ++index) {
-                options += "<option value='" + SearchResult[index]["p_status_code_c"] + "'>" +
-                    SearchResult[index]["p_status_code_c"] + "</option>";
-            }
-            $("#partnerstatus").html(options);
-        }
-        else
-        {
-            alert("something went wrong");
-        }
-      },
-      error: function(response, status, error) {
-        console.debug(error);
-        console.debug(JSON.stringify(response.responseJSON));
-        alert("Server error, please try again later");
-      },
-      fail: function(msg) {
-        console.debug(msg);
-        alert("Server failure, please try again later");
-      }
-    });
+ComboboxInitValues(
+                    "serverMPartner.asmx/TPartnerCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'PartnerStatusList',
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_status_code_c",
+                    "p_status_code_c",
+                    "#partnerstatus");
 
 // Country
-    $.ajax({
-      type: "POST",
-      url: "serverMCommon.asmx/TCommonCacheableWebConnector_GetCacheableTable",
-      data: JSON.stringify({
-            'ACacheableTable': 'CountryList',
-            'AHashCode': '',
-            }),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data, status, response) {
-        result = JSON.parse(response.responseText);
-        //console.debug(JSON.stringify(response.responseText));
-        //console.debug(JSON.stringify(result));
-        if (result['d'] != 0)
-        {
-            //console.debug(JSON.stringify(result['d']));
-            //console.debug(JSON.stringify(JSON.parse(result['d'])));
+ComboboxInitValues(
+                    "serverMCommon.asmx/TCommonCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'CountryList',
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_country_code_c",
+                    "p_country_name_c",
+                    "#country");
 
-            SearchResult = JSON.parse(result['d'])[1];
+// Location Type
+ComboboxInitValues(
+                    "serverMPartner.asmx/TPartnerCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'LocationTypeList',
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_code_c",
+                    "p_code_c",
+                    "#locationtype");
+                    
+// Publication Code
+ComboboxInitValues(
+                    "serverMPartner.asmx/TSubscriptionsCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'PublicationList',
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_publication_code_c",
+                    "p_publication_code_c",
+                    "#publicationcode");  
 
-            ParsedSearchResult = new Array();
-
-            options = "<option></option>";
-            for (index = 0; index < SearchResult.length; ++index) {
-                options += "<option value='" + SearchResult[index]["p_country_code_c"] + "'>" +
-                    SearchResult[index]["p_country_name_c"] + "</option>";
-            }
-            $("#country").html(options);
-        }
-        else
-        {
-            alert("something went wrong");
-        }
-      },
-      error: function(response, status, error) {
-        console.debug(error);
-        console.debug(JSON.stringify(response.responseJSON));
-        alert("Server error, please try again later");
-      },
-      fail: function(msg) {
-        console.debug(msg);
-        alert("Server failure, please try again later");
-      }
-    });
-
-// Location Type    
-    $.ajax({
-      type: "POST",
-      url: "serverMPartner.asmx/TPartnerCacheableWebConnector_GetCacheableTable",
-      data: JSON.stringify({
-            'ACacheableTable': 'LocationTypeList',
-            'AHashCode': '',
-            }),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data, status, response) {
-        result = JSON.parse(response.responseText);
-        //console.debug(JSON.stringify(response.responseText));
-        //console.debug(JSON.stringify(result));
-        if (result['d'] != 0)
-        {
-            //console.debug(JSON.stringify(result['d']));
-            //console.debug(JSON.stringify(JSON.parse(result['d'])));
-
-            SearchResult = JSON.parse(result['d'])[1];
-
-            ParsedSearchResult = new Array();
-
-            options = "";
-            for (index = 0; index < SearchResult.length; ++index) {
-                options += "<option value='" + SearchResult[index]["p_code_c"] + "'>" +
-                    SearchResult[index]["p_code_c"] + "</option>";
-            }
-            $("#locationtype").html(options);
-        }
-        else
-        {
-            alert("something went wrong");
-        }
-      },
-      error: function(response, status, error) {
-        console.debug(error);
-        console.debug(JSON.stringify(response.responseJSON));
-        alert("Server error, please try again later");
-      },
-      fail: function(msg) {
-        console.debug(msg);
-        alert("Server failure, please try again later");
-      }
-    });
+// Reason Given
+ComboboxInitValues(
+                    "serverMPartner.asmx/TSubscriptionsCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'ReasonSubscriptionGivenList',
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_code_c",
+                    "p_code_c",
+                    "#reasongiven"); 
+                    
+// Reason Ended
+ComboboxInitValues(
+                    "serverMPartner.asmx/TSubscriptionsCacheableWebConnector_GetCacheableTable",
+                    {
+                    'ACacheableTable': 'ReasonSubscriptionCancelledList',
+                    'AHashCode': '',
+                    },
+                    true, // AWithEmptyOption
+                    "p_code_c",
+                    "p_code_c",
+                    "#reasonended");                    
+  
 }
